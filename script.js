2fe17c896885a26ac450749ebb1353ec0a5c9a83
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2019-11-30 01:03:59 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// var APIKey = "fa934da5077bdb44ec234f60ae986bdd";
+
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+//   "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
+
+
+
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).then(function(response) {
+
+//     console.log(queryURL);
+
+//     console.log(response);
+// });
