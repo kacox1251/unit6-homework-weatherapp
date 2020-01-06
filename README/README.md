1de47520c46d91a476ebb57cfdcbbcf2c9fcf38e
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-01-06 04:55:58 -0700

    js overhaul

--- README/README.md
+++ README/README.md
@@ -1,1 +1,52 @@
 # unit6-homework-weatherapp
+
+## Description
+
+This is the weather for the Unit 6 homework.
+Open Weather API was a beast to get through. There is so much information packed into it.
+Future Features:
+    -Local storage saves your last searched cities
+    -Front page shows your current location's weather
+
+## Installation
+
+In order to install this project follow the Github link to https://github.com/kacox1251/unit6-homework-weatherapp. After that clone the repository through your Git Bash. To do that type 'git clone" and paste the HTML link inside your terminal, it will ask you for your github password. After the repository is saved in the directory of your choice you can access the files and view them in your default browser.
+You can also download as a zip file by clicking "Download zip" in the "Clone or Download" dropdown.
+You can also see the live site by following the link to https://kacox1251.github.io/unit6-homework-weatherapp/.
+
+## Usage
+
+This page allows you to search for and see the weather in multipple cities. Type a city name into the search bar and a button list will generate. Clicking on the buttons will pull up information about each city's weather on that day. It will also show a 5 day forecast.
+
+## Credits
+
+Credit to this project belongs to me.
+Icons belong to https://fontawesome.com/icons?from=io
+Styling belings to https://getbootstrap.com/
+Fonts belong to https://fonts.google.com/
+https://jquery.com/ was used
+All times utilized https://momentjs.com/
+
+## Liscense
+
+MIT License
+
+Copyright (c) [2019] [Krystal Cox]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,35 +1,159 @@
-var APIKey = "166a433c57516f51dfab1f7edaed8413";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Denver&units=imperial&appid=" + APIKey;
+var cities = [];
+var tempEl = $("#city-temp");
+var humidEl = $("#city-humidity");
+var windEl = $("#city-wind");
+var uvEl = $("#city-uv");
+var cityNameEl = $(".current-city-name");
+var currentDateEl = $(".current-date");
+var weatherIconEl = $(".weather-icon");
 
+var currentCity = "";
 
-$(".city-search").on("click", function(){
-    var cityName = $(".city-name").val();
-    $(".list-group").html(`<button type="button" class="list-group-item list-group-item-action city"></button>`);
-    $(".city").text(cityName);
-    console.log(cityName);
-})
+var currentDate = moment().format("MM/DD/YY");
 
+var apiKey = "166a433c57516f51dfab1f7edaed8413";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    console.log(queryURL);
-    console.log(response);
-    $(".current-city-name").text(response.name);
-    $(".current-date").text("today's date");
-    $(".weather-icon").text(response.weather.icon);
-    $(".city-temp").text("Temperature: " + response.main.temp);
-    $(".city-humidity").text("Humidity: " + response.main.humidity);
-    $(".city-wind").text("Wind Speed: " + response.wind.speed);
-    $(".city-uv").text("UV Index: ");
+// currentCityWeather();
+
+$(".city-search").on("click", function(event){
+    event.preventDefault();
+    $(".list-group").empty();
+    
+    var cityName = $(".city-name").val().trim();
+    cities.push(cityName);
+    $(".city-name").val("");
+
+    for (var i = 0; i < cities.length; i++) {
+        var button = $("<buttons>");
+        button.addClass("btn btn-primary animal mr-2 city-button");
+        button.text(cities[i]);
+        button.attr("data-city", cities[i]);
+        $(".list-group").prepend(button);
+    }
+
+    var cityBtn = $(".city-button");
+    var cityBtnText = $(".city-button").attr("data-city");
+    var cityString = JSON.stringify(cityBtnText);
+    // localStorage.setItem(response.name, JSON.stringify($("#all-container").html()))
+
+    cityBtn.on("click", function(event) {
+        event.preventDefault();
+        currentDateEl.text(currentDate);
+        var clickedCity = $(this).attr("data-city");
+        cityNameEl.text(clickedCity);
+        var city = cityNameEl.text();
+        console.log(city)
+        
+        var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+        var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+        var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&appid=" + apiKey;
+
+        $.ajax({
+            url: weatherQueryURL,
+            method: "GET"  
+        }).then(function(response) {
+            tempEl.text("Tempurature: " + response.main.temp + "°F");
+            humidEl.text("Humidity: " + response.main.humidity);
+            windEl.text("Wind Speed: " + response.wind.speed + "mph");
+            var iconCode = response.weather[0].icon
+            var icon = "http://openweathermap.org/img/w/" + iconCode + ".png";
+            weatherIconEl.attr("src", icon);
+        });
+        
+        $.ajax({
+            url: forecastQueryURL,
+            method: "GET"  
+        }).then(function(response) {
+            console.log(response)
+            for (var i = 1; i < 6; i++) {
+                var fiveDayCard = $("<div>");
+                fiveDayCard.addClass("card text-white bg-primary mb-3 five-day");
+                fiveDayCard.attr("style", "max-width: 18rem;");
+                
+                var date = $("<div>")
+                date.addClass("card-header date");
+                var fiveDay = moment().add(i, "days").format('l')
+                date.text(fiveDay);
+                fiveDayCard.append(date);
+                
+                var fiveDayInfo = $("<div>");
+                fiveDayInfo.addClass("card-body")
+                fiveDayCard.append(fiveDayInfo);
+                
+                var fiveDayIcon = $("<img>");
+                fiveDayIcon.addClass("five-day-icon")
+                fiveDayInfo.append(fiveDayIcon)
+                
+                var fiveDayTemp = $("<p>");
+                fiveDayTemp.addClass("five-day-temp")
+                fiveDayInfo.append(fiveDayTemp);
+                
+                var fiveDayHumid = $("<p>");
+                fiveDayHumid.addClass("five-day-humid")
+                fiveDayInfo.append(fiveDayHumid);
+
+                fiveDayCard.append(date);
+                fiveDayCard.append(fiveDayInfo);
+                $(".card-row").append(fiveDayCard);
+            };
+
+            $(".five-day-icon").eq(0).attr("src", `http://openweathermap.org/img/w/${response.list[4].weather[0].icon}.png`);
+            $(".five-day-icon").eq(1).attr("src", `http://openweathermap.org/img/w/${response.list[12].weather[0].icon}.png`);
+            $(".five-day-icon").eq(2).attr("src", `http://openweathermap.org/img/w/${response.list[20].weather[0].icon}.png`);
+            $(".five-day-icon").eq(3).attr("src", `http://openweathermap.org/img/w/${response.list[28].weather[0].icon}.png`);
+            $(".five-day-icon").eq(4).attr("src", `http://openweathermap.org/img/w/${response.list[36].weather[0].icon}.png`);
+
+            $(".five-day-temp").eq(0).text(`Temperature: ${((response.list[4].main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`);
+            $(".five-day-temp").eq(1).text(`Temperature: ${((response.list[12].main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`);
+            $(".five-day-temp").eq(2).text(`Temperature: ${((response.list[20].main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`);
+            $(".five-day-temp").eq(3).text(`Temperature: ${((response.list[28].main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`);
+            $(".five-day-temp").eq(4).text(`Temperature: ${((response.list[36].main.temp - 273.15) * 1.80 + 32).toFixed(2)}°F`);
+
+            $(".five-day-humid").eq(0).text(`Humidity: ${response.list[4].main.humidity}`);
+            $(".five-day-humid").eq(1).text(`Humidity: ${response.list[12].main.humidity}`);
+            $(".five-day-humid").eq(2).text(`Humidity: ${response.list[20].main.humidity}`);
+            $(".five-day-humid").eq(3).text(`Humidity: ${response.list[28].main.humidity}`);
+            $(".five-day-humid").eq(4).text(`Humidity: ${response.list[36].main.humidity}`);
+        });
+        
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"  
+        }).then(function(response) {
+            uvEl.text("UV Index: " + response.value)
+        });
+    });
 });
 
-if (navigator.geolocation) {
-    console.log("yay")
-}
-else {
-    console.log("uh oh")
-}
+// function currentCityWeather() {
+//     var apiKey = "166a433c57516f51dfab1f7edaed8413";
+//     var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+//     var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+//     var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&appid=" + apiKey;
 
-//                    <button type="button" class="list-group-item list-group-item-action"></button>
+//     $.ajax({
+//         url: weatherQueryURL,
+//         method: "GET"  
+//     }).then(function(response) {
+//         console.log(response);
+//         tempEl.text("Tempurature: " + response.main.temp);
+//         humidEl.text("Humidity: " + response.main.humidity);
+//         windEl.text("Wind Speed: " + response.wind.speed);
+//         weatherIconEl.html()
+//     });
+    
+//     $.ajax({
+//         url: forecastQueryURL,
+//         method: "GET"  
+//     }).then(function(response) {
+//         console.log(response);
+//     });
+    
+//     $.ajax({
+//         url: uvQueryURL,
+//         method: "GET"  
+//     }).then(function(response) {
+//         console.log(response);
+//         uvEl.text("UV Index: " + response.value)
+//     });
+// }
